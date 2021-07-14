4af4f250b13fddab9f913a5f168bfe2541d53d2a
scaraclette <gustiscarlett@gmail.com>
2021-07-13 22:57:53 -0700

    tuesday

--- sliding_window/introduction.py
+++ sliding_window/introduction.py
@@ -27,6 +27,19 @@ def optimized_find_averages(k, arr):
     return result
 
 # Grokking solution
+def solution_find_averages(k, arr):
+    result = []
+    windowSum, windowStart = 0.0, 0
+
+    for windowEnd in range(len(arr)):
+        windowSum += arr[windowEnd]
+
+        if windowEnd >= k - 1:
+            result.append(windowSum / k)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+    
+    return result
 
 def main():
   result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
